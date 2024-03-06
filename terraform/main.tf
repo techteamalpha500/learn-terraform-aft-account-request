5653ef7f9dd3b5c19f5b9c2846d83299be9aa797
techteam <arunclement1505@gmail.com>
2024-03-06 14:02:00 +0530

    Adding new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "testaudit87689@gmail.com"
-    AccountName               = "CentralNetwork"
+    AccountEmail              = "garunclement125@gmail.com"
+    AccountName               = "network-aft"
     ManagedOrganizationalUnit = "SharedServices"
-    SSOUserEmail              = "testaudit87689@gmail.com"
-    SSOUserFirstName          = "CentralNetwork"
+    SSOUserEmail              = "garunclement125@gmail.com"
+    SSOUserFirstName          = "network"
     SSOUserLastName           = "AFT"
   }
 
@@ -16,12 +16,12 @@ module "sandbox" {
 
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_reason       = "Creating new account"
   }
 
   custom_fields = {
     group = "non-prod"
   }
 
-  account_customizations_name = "CentralNetwork"
+  account_customizations_name = "network"
 }
